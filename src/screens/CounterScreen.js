3d6b4d8174f3ac33396fd641358aeecdd9daed88
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-18 18:38:04 +0300

    useReducer exercise

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,5 +1,37 @@
-import React , {useState} from "react";
+import React , {useState,useReducer} from "react";
 import {Text,View,StyleSheet,Button} from "react-native";
+
+/*
+const COUNT_INCREMENT = 1;
+
+const reducer = (state,action) => {
+
+    switch(action.type){
+        case 'Increase':
+            return {...state , count : state.count + action.payload};
+        case 'Decrease':
+            return {...state , count : state.count - action.payload}; 
+        default:
+            return state;
+    }
+
+};
+
+const CounterScreen = () => {
+
+    const [state,dispatch] = useReducer(reducer,{count : 0});
+
+    return (
+        <View>
+            <Button title = "Increase" onPress = { () => dispatch({type:'Increase',payload:COUNT_INCREMENT})}/>
+            <Button title = "Decrease" onPress = { () => dispatch({type:'Decrease',payload:COUNT_INCREMENT})}/>
+            <Text>Current Count: {state.count}</Text>
+        </View>
+    );
+};
+*/
+
+
 
 const CounterScreen = () => {
     const [counter,setCounter] = useState(0);
